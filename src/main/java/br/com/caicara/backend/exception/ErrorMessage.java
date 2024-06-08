2e2f7a99a5874ebcbfe677c9c376ef09c4a72427
Lucas Bernadino <luucasdinoo@gmail.com>
2024-06-08 20:52:32 -0300

    Configuração do spring security

---
+++ src/main/java/br/com/caicara/backend/exception/ErrorMessage.java
@@ -0,0 +1,48 @@
+package br.com.caicara.backend.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @NoArgsConstructor
+public class ErrorMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> erros;
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErros(result);
+    }
+
+    private void addErros(BindingResult result) {
+        this.erros = new HashMap<>();
+        for (FieldError fieldError : result.getFieldErrors()){
+            this.erros.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
